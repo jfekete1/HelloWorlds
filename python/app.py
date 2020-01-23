d629230785097bc91a997789782bc3075ba2abc8
Jozsef Fekete <44202895+jfekete1@users.noreply.github.com>
2020-01-23 10:06:50 +0100

    Add files via upload

---
+++ python/app.py
@@ -0,0 +1,22 @@
+hi = "Hello World !!"
+hi = hi.replace('H', 'Y')
+print(hi)
+print('World' in hi)
+price = 10
+print(price)
+print('*' * 10)
+onGoingShit = input("Birth year: ")
+asdf = 2020 - abs(int(onGoingShit))
+print("You gonna be " + str(asdf) + " years old this year motherfucker !!")
+
+is_hot = True
+is_cold = False
+
+if (is_hot and not is_cold):
+    print("It's hot mofo !!")
+    print('go drink somethang')
+elif is_cold and not is_hot:
+    print('It\'t cold biatch')
+else:
+    print('haggyukmá')
+print('bye !!')
